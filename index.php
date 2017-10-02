8d0704003f3b5d08514b8ee19120b364646ec1a2
salmhassan <32253403+salmhassan@users.noreply.github.com>
2017-10-02 02:52:57 -0400

    edit in coding
    i added and edit in my code, i added the start session and added the counter function in the code and i found a code that made my code can log in and log out.

--- index.php
+++ index.php
@@ -1,73 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-body {
-    background-color: darkred;
-}
-
-h1 {
-    color: white;
-    text-align: center;
-     font-size: 40px;
-}
-
-p {
-	 color: darkgrey;
-    	font-family: verdana;
- 	 	font-size: 20px;
-        text-align: center;
-}
-
-m{
-align: center;
-}
-
-
-</style>
-</head>
-
-<h1> Salman Hassan </h1>
-<p>This is Assignment 1</p>
-
-
-
-<html>
-<head> 
-<title> Login </title>
-<body>
-
-	<form action ="#" method = "post">
-	Username ID 
-	<input type = "text" name = "username">
-	<br>
-	Password 
-	<input type = "password" name = "pass">
-	
-	<br>
-	<input type = "submit" value = " Login" name = "submit">
-</form>
-</body>
-</html>
-
-
-<?php
-
-	if(isset($_POST['submit']))
-	{
-		$user = $_POST['username'];
-		$pass = $_POST['pass'];
-		if(($user == "SalmanHassan") && ($pass = "55555")){
-			echo "Welcome Salman";
-		}
-		
-		else if(($user == "SultanHassan") && ($pass = "123456")){
-			echo "Welcome Sultan";
-		}
-		
-	else {
-			echo "invalid username / Password" ;
-	}
-	}
-
-?>
+<!DOCTYPE HTML><html>	<head>			<style>					body {				background-color: darkred;			}			h1 {				color: white;				text-align: center;				font-size: 40px;			}			h2 {				color: darkgrey;				font-family: verdana;				font-size: 20px;				text-align: center;			}			p {				color: black;				font-family: verdana;				font-size: 20px;				text-align: center;			}</style><h1> Salman Hassan </h1><h2>This is Assignment 1</h2>    <head>	        <title> Login </title>    </head>        <body>                <form method="post" action="index.php">           <p> <label for="username">Username:</label><br/> </p>           <p> <input type="text" name="username" id="username"><br/> </p>           <p>  <label for="password">Password:</label><br/> </p>           <p>  <input type="password" name="password" id="password"><br/> </p>           <p> <input type="submit" value="Log In!"> </p>        </form>    </body></html><?php	$_SESSION['fail'] = 0;	// We start the session from this point.    session_start();    // Add the usernames into an array.    $username = array("SalmanHassan","SultanHassan");	// Add the passwodrds into another array.    $password = array("55555","12345");    // Show the error.    $error = "";	// See wheather the user is logged in or not, and move him to the other page if he logged in.    if (isset($_SESSION['login']) && $_SESSION['login'] == true) {        $error = "approved";				// Attatch it to the approved code.        header('Location: approved.php');    }         	// See wheather the information entered, then log in.    if (isset($_POST['username']) && isset($_POST['password'])) {        if ($_POST['username'] == $username[0] && $_POST['password'] == $password[0]) {            $_SESSION['login'] = true;            header('Location: approved.php');        } 		else if($_POST['username'] == $username[1] && $_POST['password'] == $password[1]) {	            $_SESSION['login'] = true;	            header('Location: approved.php');		}else {            $_SESSION['login'] = false;			$_SESSION['fail'] = $_SESSION['fail'] + 1;            $error = "You have entered incorrect information  ".$_SESSION['fail'];			 			        }    }?>		        <?php		// Show the error.		echo $error; ?>
